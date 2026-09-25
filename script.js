2dcde852eeaa7fd24c4fd4de68125ebe6fc53ea0
Pedro Lucas Da Silva Rosa <pedrolucass1106@gmail.com>
2026-09-24 21:30:38 -0300

    Refactor loadPhoto and notification handling

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const valorComprovantes = document.getElementById('valor_comprovantes')
 const quantidadeComprovante = document.getElementById('quantidade_comprovantes')
 const flashedBox = document.getElementById('flashed_box')
 
+const avisoIA = document.getElementById('aviso-ia')
+
 let comprovantes_loaded = 0
 let valor_total_comprovantes = 0
 
@@ -56,6 +58,8 @@ async function loadPhoto() {
             { "nome": "Nome do produto", "preco": 85.00, "quantidade": 2 }
         ]
     }`, comprovante)
+
+    avisoIA.textContent = ''
 
     console.log(resposta.message.content)
     let dados = JSON.parse(resposta.message.content)
